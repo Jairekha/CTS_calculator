3fd86e76546a6db43b9ddc713c6c6858573eaa1b
abhi22112002 <93024247+abhi22112002@users.noreply.github.com>
2024-04-04 16:43:18 +0530

    Update calc_3.py

--- calc_3.py
+++ calc_3.py
@@ -10,3 +10,16 @@ buttons = [
 
 row_num = 1
 col_num = 0
+
+for button in buttons:
+    if button == '=':
+        tk.Button(window, text=button, width=10, command=calculate).grid(row=row_num, column=col_num, padx=5, pady=5)
+    elif button == 'C':
+        tk.Button(window, text=button, width=10, command=clear_all).grid(row=row_num, column=3, padx=5, pady=5)
+    else:
+        tk.Button(window, text=button, width=10, command=lambda b=button: add_to_display(b)).grid(row=row_num, column=col_num, padx=5, pady=5)
+    
+    col_num += 1
+    if col_num > 3:
+        col_num = 0
+        row_num += 1
