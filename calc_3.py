fdc429a32559066d32a5b9252cdb2217ef9ea5e8
Jaichandran G <107838548+Jairekha@users.noreply.github.com>
2024-04-04 16:41:25 +0530

    Add files via upload

---
+++ calc_3.py
@@ -0,0 +1,12 @@
+3rd part
+
+# Create number buttons
+buttons = [
+    '7', '8', '9', '/',
+    '4', '5', '6', '*',
+    '1', '2', '3', '-',
+    '0', '=', '+','C'  # Added '0' button
+]
+
+row_num = 1
+col_num = 0
